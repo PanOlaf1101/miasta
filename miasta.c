db0ecd414d0986ad8d401075c84a83dae37d5018
Olaf Mianowski <olafmianowski@MacBook-Pro-Olaf.local>
2021-06-18 16:15:34 +0200

    Mała poprawka :)

--- miasta.c
+++ miasta.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-char litery[] = "ABCDEFGHIJKLMNOPRSTUWZ";
 typedef unsigned short us;
-us ile_liter;
+char litery[] = "ABCDEFGHIJKLMNOPRSTUWZ";
 const size_t size = sizeof(litery)-1;
 void mix() {
+	srand(time(NULL) ^ rand());
 	char temp;
 	int random;
 	for(us i=0; i<size; i++) {
@@ -13,22 +13,20 @@ void mix() {
 		temp = litery[random];
 		litery[random] = litery[i];
 		litery[i] = temp;
-
 	}
 }
 int main(int argc, char** argv) {
-	srand(time(NULL) ^ rand());
 	mix();
-	us* gracze = (us*) calloc(sizeof(us), argc-2);
-	us los;
-	us ile_tur = atoi(argv[1]);
-	argc-=2;
+	us ile_tur = atoi(*(argv+1));
+	argc -= 2;
 	argv += 2;
+	us* gracze = (us*) calloc(sizeof(us), argc);
+	us los;
 	ile_tur = (ile_tur > size)? size : ile_tur;
 	for(us i=0; i<ile_tur; i++) {
 		printf("Wylosowana litera to %c\n", litery[i]);
 		for(us j=0; j<argc; j++) {
-			printf("Ile %s ma teraz punktow? ", *(argv+j));
+			printf("Ile %s ma w tej rundzie punktow? ", *(argv+j));
 			scanf("%hu", &los);
 			*(gracze+j) += los;
 		}
